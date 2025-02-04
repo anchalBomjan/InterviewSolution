50aaa2528f5fbb6ad5d22e41a05f8eb94e5fbc9d
anchalBomjan <anchal.percoidit@gmail.com>
2025-02-04 23:13:22 +0545

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.

---
+++ Basic math/sum.app/sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+public class Solution
+{
+    public  int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+
+            }
+            return Array.Empty<int>();
+        }
+        return Array.Empty<int>();
+    }
+
+    // Entry point of the program
+    class Program
+    {
+        static void Main()
+        {
+            Solution solution = new();
+            
+
+            int[] result = solution.TwoSum(new int[] {2,7,11,15},9);
+
+            Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+        }
+    }
+}
