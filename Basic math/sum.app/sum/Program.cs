e2c07357b24e42939dd2f2c78cd8f0a5656a02a0
anchalBomjan <anchal.percoidit@gmail.com>
2025-02-05 15:07:28 +0545

    Modifying the code to fullfilled timeComplexity

--- Basic math/sum.app/sum/Program.cs
+++ Basic math/sum.app/sum/Program.cs
@@ -1,37 +1,68 @@
-﻿using System;
+﻿//This method is bruteforce approached to solved  the problems which have time complexity is O(n)^2
 
 
+//using System;
+//public class Solution
+//{
+//    public  int[] TwoSum(int[] nums, int target)
+//    {
+//        //time Complexity is O(n)^2
+//        for (int i = 0; i < nums.Length; i++)
+//        {
+//            for (int j = i + 1; j < nums.Length; j++)
+//            {
+//                if (nums[i] + nums[j] == target)
+//                {
+//                    return new int[] { i, j };
+//                }
+
+//            }
+
+//        }
+//        return Array.Empty<int>();
+//    }
+
+//    // Entry point of the program
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Solution solution = new();
+
+
+//            int[] result = solution.TwoSum(new int[] {2,7,11,15},9);
+
+//            Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+//        }
+//    }
+//}
 public class Solution
 {
-    public  int[] TwoSum(int[] nums, int target)
+    public int[] TwoSum(int[] nums, int targets)
     {
-        //time Complex  is n(0)^2
+        //Dictionary is used to store the numbers and  their indices
+        Dictionary<int, int> numToIndex = new Dictionary<int, int>();
         for (int i = 0; i < nums.Length; i++)
         {
-            for (int j = i + 1; j < nums.Length; j++)
+            int complement = targets - nums[i];
+            //checking wheather the element(complement) is avaiable or not in ArrayList(Dictionary)
+            if (numToIndex.ContainsKey(complement))
             {
-                if (nums[i] + nums[j] == target)
-                {
-                    return new int[] { i, j };
-                }
-
+                return new int[] { numToIndex[complement], i };
             }
-            
+            //other wise loops will run untill its lenghts
+            numToIndex[nums[i]] = i;
         }
         return Array.Empty<int>();
+
     }
-
-    // Entry point of the program
-    class Program
+}
+class Program
+{
+    public static void Main(string[] args)
     {
-        static void Main()
-        {
-            Solution solution = new();
-            
-
-            int[] result = solution.TwoSum(new int[] {2,7,11,15},9);
-
-            Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
-        }
+        Solution solution = new Solution();
+        int[] result = solution.TwoSum(new int[] { 2, 7, 11, 15 }, 18);
+        Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
     }
 }
--- Basic math/sum.app/sum/obj/Debug/net8.0/sum.AssemblyInfo.cs
+++ Basic math/sum.app/sum/obj/Debug/net8.0/sum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50aaa2528f5fbb6ad5d22e41a05f8eb94e5fbc9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0b5d128a0f563170b09e0d0c333e4bc85cd365")]
 [assembly: System.Reflection.AssemblyProductAttribute("sum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
